bec96ebf7e99f51aa71fe102693759ca1cda31c8
tahaturan <tahaaturann16@gmail.com>
2023-10-08 22:31:44 +0300

    StringConstant eklendi

---
+++ CoffeeConnect/Utility/Constants/StringConstants.swift
@@ -0,0 +1,15 @@
+//
+//  StringConstants.swift
+//  CoffeeConnect
+//
+//  Created by Taha Turan on 8.10.2023.
+//
+
+import Foundation
+
+struct StringConstants {
+    struct Login {
+        static let emailPlaceholder = NSLocalizedString("E-mail", comment: "")
+        static let passwordPlaceholder = NSLocalizedString("Password", comment: "")
+    }
+}
---
+++ CoffeeConnect/View/CustomTextField.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTextField.swift
+//  CoffeeConnect
+//
+//  Created by Taha Turan on 8.10.2023.
+//
+
+import Foundation
