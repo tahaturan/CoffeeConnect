aaa52679d5de71885e977d6affaf20915b1b1323
tahaturan <tahaaturann16@gmail.com>
2023-10-10 11:54:18 +0300

    ViewController icin alert extesion u eklendi

--- CoffeeConnect/Utility/Extensions/AppExtensions.swift
+++ CoffeeConnect/Utility/Extensions/AppExtensions.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+//MARK: - AlertController
+extension UIViewController {
+    func showAlert(title: String, message: String, buttonTitle: String = StringConstants.AppString.okString, completion: (() -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.view.tintColor = AppColors.ambassadorBlue.color
+        alertController.view.layer.cornerRadius = AppStyle.defaultCornerRadius
+        let okAction = UIAlertAction(title: buttonTitle, style: .default) { _ in
+            completion?()
+        }
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
