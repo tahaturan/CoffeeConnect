cf0b5a79f5fed3c614bda825402e349bd86cc09a
tahaturan <tahaaturann16@gmail.com>
2023-10-10 23:35:27 +0300

    FireBase Servis sinifi eklendi

--- CoffeeConnect/Service/FirebaseService.swift
+++ CoffeeConnect/Service/FirebaseService.swift
@@ -6,14 +6,19 @@
 //
 
 import FirebaseAuth
+import FirebaseStorage
+import FirebaseFirestore
 
-class FirebaseAuthService {
-    static let shared = FirebaseAuthService()
+class FirebaseService {
+    // Singleton pattern ile sadece bir örnek oluşturulmasını sağlıyoruz.
+    static let shared = FirebaseService()
     
     private init() {}
     
-    // Kullanıcı Kaydı
-    func signUp(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+    // MARK: - User SignUp
+    
+    func signUp(email: String, password: String, name: String, username: String, profileImage: UIImage, completion: @escaping (Result<User, Error>) -> Void) {
+        // 1. Kullanıcıyı Firebase Authentication ile kaydediyoruz.
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             if let error = error {
                 completion(.failure(AppError.custom(error.localizedDescription)))
@@ -25,11 +30,67 @@ class FirebaseAuthService {
                 return
             }
             
-            completion(.success(user))
+            // 2. Kullanıcının profil fotoğrafını Firebase Storage'a kaydediyoruz.
+            self.uploadProfileImage(user: user, image: profileImage) { result in
+                switch result {
+                case .success(let profileImageUrl):
+                    // 3. Kullanıcının bilgilerini Firestore'a kaydediyoruz.
+                    self.saveUserToFirestore(user: user, name: name, username: username, email: email, profileImageUrl: profileImageUrl, completion: completion)
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
         }
     }
     
-    // Kullanıcı Girişi
+    // Profil fotoğrafını yüklemek için
+    private func uploadProfileImage(user: User, image: UIImage, completion: @escaping (Result<String, Error>) -> Void) {
+        let storageRef = Storage.storage().reference().child("profile_images").child("\(user.uid).jpg")
+        if let uploadData = image.jpegData(compressionQuality: 0.75) {
+            storageRef.putData(uploadData, metadata: nil) { _, error in
+                if let error = error {
+                    completion(.failure(AppError.custom(error.localizedDescription)))
+                    return
+                }
+                
+                storageRef.downloadURL { url, error in
+                    if let error = error {
+                        completion(.failure(AppError.custom(error.localizedDescription)))
+                        return
+                    }
+                    
+                    guard let profileImageUrl = url?.absoluteString else {
+                        completion(.failure(AppError.profileImageURLNotFound))
+                        return
+                    }
+                    
+                    completion(.success(profileImageUrl))
+                }
+            }
+        }
+    }
+    
+    // Kullanıcı bilgilerini Firestore'a kaydetmek için
+    private func saveUserToFirestore(user: User, name: String, username: String, email: String, profileImageUrl: String, completion: @escaping (Result<User, Error>) -> Void) {
+        let newUser = UserModel(userID: user.uid, name: name, username: username, email: email, balance: 0.0, profileImageURL: profileImageUrl, postIDs: [], shoppingCart: ShoppingCartModel(userID: user.uid, items: []), wishlist: [])
+        
+        let dbRef = Firestore.firestore().collection("users").document(user.uid)
+        do {
+            let userData = try newUser.toDictionary()
+            dbRef.setData(userData) { error in
+                if let error = error {
+                    completion(.failure(AppError.custom(error.localizedDescription)))
+                } else {
+                    completion(.success(user))
+                }
+            }
+        } catch {
+            completion(.failure(AppError.dataEncodingFailed))
+        }
+    }
+    
+    // MARK: - User SignIn
+    
     func signIn(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
             if let error = error {
@@ -42,14 +103,41 @@ class FirebaseAuthService {
                 return
             }
             
-            completion(.success(user))
+            // Kullanıcı Bilgilerini Firestore'dan alıyoruz
+            self.fetchUserFromFirestore(user: user, completion: completion)
         }
     }
     
-    // Kullanıcı Çıkışı
+    // Kullanıcı bilgilerini Firestore'dan almak için
+    private func fetchUserFromFirestore(user: User, completion: @escaping (Result<User, Error>) -> Void) {
+        let dbRef = Firestore.firestore().collection("users").document(user.uid)
+        dbRef.getDocument { document, error in
+            if let error = error {
+                completion(.failure(AppError.custom(error.localizedDescription)))
+                return
+            }
+            
+            if let userDocument = document, userDocument.exists {
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: userDocument.data()!)
+                    let loggedInUser = try JSONDecoder().decode(UserModel.self, from: jsonData)
+                    UserManager.shared.updateUser(loggedInUser)
+                    completion(.success(user))
+                } catch {
+                    completion(.failure(AppError.dataDecodingFailed))
+                }
+            } else {
+                completion(.failure(AppError.userDocumentNotFound))
+            }
+        }
+    }
+    
+    // MARK: - User SignOut
+    
     func signOut() -> Bool {
         do {
             try Auth.auth().signOut()
+            UserManager.shared.clearUser()
             return true
         } catch {
             print("Sign out error: \(error.localizedDescription)")
@@ -57,7 +145,8 @@ class FirebaseAuthService {
         }
     }
     
-    // Şu anki kullanıcıyı al
+    // MARK: - CurrentUser
+
     var currentUser: User? {
         return Auth.auth().currentUser
     }
