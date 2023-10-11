695610b93f7de4592d952d53539877ba6a937df5
tahaturan <tahaaturann16@gmail.com>
2023-10-11 22:53:09 +0300

    CoffeModel ve CoffeeCategoryModel Codeble yapisina uygun sekilde Decode ve Encode islemleri eklendi

--- CoffeeConnect/Model/CoffeeCategoryModel.swift
+++ CoffeeConnect/Model/CoffeeCategoryModel.swift
@@ -9,5 +9,31 @@ import Foundation
 
 struct CoffeeCategoryModel: Codable {
     let categoryID: String
+    let categoryName: String
     var coffeeIDs: [String]     // Kategorideki kahvelerin ID'leri
+    
+    struct CoffeeCategoryModel: Codable {
+        let categoryID: String
+        let categoryName: String
+        var coffeeIDs: [String]     // Kategorideki kahvelerin ID'leri
+        
+        // CoffeeCategoryModel objesini Dictionary'e dönüştürme
+        func toDictionary() throws -> [String: Any] {
+            let data = try JSONEncoder().encode(self)
+            guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+                throw AppError.dataEncodingFailed
+            }
+            return dictionary
+        }
+        
+        // JSON Data'dan CoffeeCategoryModel objesine dönüştürme
+        static func fromDictionary(jsonData: Data) throws -> CoffeeCategoryModel {
+            do {
+                return try JSONDecoder().decode(CoffeeCategoryModel.self, from: jsonData)
+            } catch {
+                throw AppError.dataDecodingFailed
+            }
+        }
+    }
+
 }
--- CoffeeConnect/Model/CoffeeModel.swift
+++ CoffeeConnect/Model/CoffeeModel.swift
@@ -14,4 +14,22 @@ struct CoffeeModel: Codable {
     var price: Double
     var imageURL: String
     var categoryID: String      // Kahvenin kategorisinin ID'si
+    
+    // CoffeeModel objesini Dictionary'e dönüştürme
+    func toDictionary() throws -> [String: Any] {
+        let data = try JSONEncoder().encode(self)
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            throw AppError.dataEncodingFailed
+        }
+        return dictionary
+    }
+    
+    // JSON Data'dan CoffeeModel objesine dönüştürme
+    static func fromDictionary(jsonData: Data) throws -> CoffeeModel {
+        do {
+            return try JSONDecoder().decode(CoffeeModel.self, from: jsonData)
+        } catch {
+            throw AppError.dataDecodingFailed
+        }
+    }
 }
